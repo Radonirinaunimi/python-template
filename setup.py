a47f6f9e51aa4c0bd05368719df2dccedb7175cb
Radonirinaunimi <tanjona.rabemananjara@unimi.it>
2020-07-10 15:21:33 +0200

    Add main file

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ if sys.version_info < (3, 6):
 
 # Read through Readme
 try:
-    with open("README.md") as f:
+    this_directory = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
         long_description = f.read()
 except IOError:
     print("Read me file not found.")
---
+++ src/package_name/run.py
@@ -0,0 +1,9 @@
+# This file should contain the main codes that controls the whole
+# behaviour of the package.
+#
+# To import modules from different files, just add here:
+# from <package_name>.module import functions, classes
+
+
+def main():
+    pass
